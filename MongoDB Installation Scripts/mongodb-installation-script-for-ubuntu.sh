e88989432f23ee6961644a6eeed45ddb36e45b84
yuvaraj <shivarajnaidu@gmail.com>
2018-08-18 07:13:26 +0530

    Improved support for ubuntu 18.04 - mongodb 4.0

--- MongoDB Installation Scripts/mongodb-installation-script-for-ubuntu.sh
+++ MongoDB Installation Scripts/mongodb-installation-script-for-ubuntu.sh
@@ -15,42 +15,35 @@ reset="$(tput sgr0)"
 bold="$(tput bold)"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+# Add Repository Source To Solurce List Based On Verion Of Ubuntu
+if [ "$(lsb_release -sr)" = "14.04" ]; then
+	echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+elif [ "$(lsb_release -sr)" = "16.04" ]; then
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+else
+	echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+fi
+
 sudo apt-get update
 
-if [ -d "/data/db/" ]; 
-	then
+if [ -d "/data/db/" ]; then
 	sudo chmod -R 777 /data/
 else
 	sudo mkdir -p /data/db/
 	sudo chmod -R 777 /data/
 fi
 
-# sudo apt-get install -y --allow-unauthenticated libcurl3
 sudo apt-get install -y --allow-unauthenticated mongodb-org
 
 if [ "$(lsb_release -sr)" = "14.04" ]; then
 	echo "$bold $green $(lsb_release -sd) $reset" 
 	sudo service mongod start
 else
-	echo "$bold $green $(lsb_release -sd) $reset" 
-	cat << EOF | sudo tee /etc/systemd/system/mongodb.service &> /dev/null
-	[Unit]
-	Description=High-performance, schema-free document-oriented database
-	After=network.target
-
-	[Service]
-	User=mongodb
-	ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
-
-	[Install]
-	WantedBy=multi-user.target
-EOF
-
-	[ "$?" = "0" ] && sudo systemctl enable mongodb
-	[ "$?" = "0" ] && sudo systemctl start mongodb || echo "$red $bold It seems something went wrong.."
-
+    sudo systemctl enable mongod
+    sudo systemctl start mongod
 fi
+
 
 echo ""
 echo "$red $bold If You Experience Any Problem While Installing MongoDB....
